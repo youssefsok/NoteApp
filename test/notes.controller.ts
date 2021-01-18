d11e7c30c0fe914b9dc2abd97dce82ed853df491
youssef <sokkah.youssef97@gmail.com>
2021-01-18 14:28:22 +0100

    Update Test

--- test/notes.controller.ts
+++ test/notes.controller.ts
@@ -3,6 +3,18 @@ import { expect } from 'chai';
 import request from 'supertest';
 import Server from '../server';
 
+var Mongoose = require('mongoose').Mongoose;
+var mongoose = new Mongoose();
+ 
+var MockMongoose = require('mock-mongoose').MockMongoose;
+var mockMongoose = new MockMongoose(mongoose);
+before(function(done) {
+  mockMongoose.prepareStorage().then(function() {
+      mongoose.connect('mongodb://example.com/TestingDB', function(err) {
+          done(err);
+      });
+  });
+});
 
 describe('notes', () => {
 
@@ -18,7 +30,7 @@ describe('notes', () => {
           .that.has.property('name')
           .equal('test');
       }));
-      
+
   it('should get all notes', () =>
     request(Server)
       .get('/api/v1/notes')
