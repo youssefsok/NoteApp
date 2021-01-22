17f8d3747799ecfb1ab1499167efc122cf922282
youssef <sokkah.youssef97@gmail.com>
2021-01-22 18:17:56 +0100

    Add load cpu

--- server/api/controllers/notes/controller.ts
+++ server/api/controllers/notes/controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import NotesServices from '../../services/notes.service';
+import L from '../../../common/logger';
 export class Controller {
   create(req: Request, res: Response): void {
     NotesServices.create(req.body.name, req.body.description).then((r) =>
@@ -8,6 +9,15 @@ export class Controller {
   }
   all(req: Request, res: Response): void {
     NotesServices.all().then((r) => res.json(r));
+  }
+
+  cpu(req: Request, res: Response): void {
+    L.info('cpu load');
+    let rand = Math.random();
+    for (let index = 1; index < 100; index++) {
+      rand *= index;
+    }
+    res.status(200).json(rand);
   }
 }
 export default new Controller();
--- server/api/controllers/notes/router.ts
+++ server/api/controllers/notes/router.ts
@@ -3,4 +3,5 @@ import controller from './controller';
 export default express
   .Router()
   .post('/', controller.create)
+  .get('/cpu', controller.cpu)
   .get('/', controller.all);
